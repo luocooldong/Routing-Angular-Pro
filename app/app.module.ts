610302c214e315e1aa4bb25da98aa157304684ea
gengyudonga@gmail.com <gengyudonga@gmail.com>
2017-08-31 19:13:05 +0800

    Preloading lazy-loaded modules

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules} from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { MailModule } from './mail/mail.module';
@@ -20,7 +20,7 @@ export const ROUTES: Routes = [
     BrowserModule,
     HttpModule,
     MailModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
   bootstrap: [
     AppComponent
